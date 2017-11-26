bbe50827d31f7b405e08b7c7ac8fcd2487284415
JamesChell <james.chell@smartrak.co.nz>
2017-11-27 10:44:25 +1300

    basic responsive nav

---
+++ toggle.js
@@ -0,0 +1,9 @@
+
+function toggle() {
+    var x = document.getElementById("responsive-menu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
---
+++ toogle.js
@@ -0,0 +1,9 @@
+
+function toggle() {
+    var x = document.getElementsByClassName("nav");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
