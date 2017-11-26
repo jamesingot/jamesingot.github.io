2e6372e853ba990bc598a14cc6c815d8c6cf341e
JamesChell <james.chell@smartrak.co.nz>
2017-11-27 10:52:21 +1300

    Nav display fix

--- toogle.js
+++
@@ -1,9 +0,0 @@
-
-function toggle() {
-    var x = document.getElementsByClassName("nav");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
